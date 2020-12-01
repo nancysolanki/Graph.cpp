f587b71e8996debda0287346e2a33827b39ee173
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2020-12-01 22:13:44 +0530

    Update has a path.cpp

--- has a path.cpp
+++ has a path.cpp
@@ -1,47 +1,71 @@
 #include <iostream>
-#include<queue>
 using namespace std;
-
-bool hasPath(int adj[][1000],int V,int a,int b)
+#include<queue>
+#include<unordered_map>
+#include<vector>
+vector<int>* bfs(int V, int adj [][1000],int v1,int v2)
 {
-   bool visited[V];
+    bool visited[V];
     for(int i=0;i<V;i++)
     {
-        visited[i]=false;
+        visited[i]=false;           
     }
     queue<int>q;
-    q.push(a);
-   visited[a]=true;
-    while(!q.empty())
-    { 
+    q.push(v1);
+    visited[v1]=true;
+    bool done=false;
+    
+  unordered_map<int,int>parent;
+    parent[v2]=v2;
+    while(!q.empty() and done==false)
+    {
         int temp=q.front();
+          q.pop();
+        for(int i=0;i<V;i++)
+        {
+            if(visited[i]==false and adj[temp][i]==1)
+            { 
+                q.push(i);
+                parent[i]=temp;
+                visited[i]=true;
+                
+            
+            if(i==v2)
+            {
+                done=true;
+                break;
+            }
+            }  
+        } 
+      
+    }  
+   if(done==false)
     
-          if(q.front()==b)
-          {
-              return true;
-          }    
-        q.pop();
-
-      for(int i=0;i<V;i++)
-      {
-          if(visited[i]==false and adj[temp][i]==1)
-          {
-              q.push(i);
-              visited[i]=true;
-          }
-       
-      }
+    {
+        return nullptr ;
     }
-    return false;
+    else
+    {
+        vector<int>* output = new vector<int>(); 
+        int current=v2;
+        output->push_back(v2);
+        while(current!=v1)
+    { 
+       
+             current=parent[current];
+             output->push_back(current);
     
+    }    return output;
+
+        }
+  
 }
-
-
-
-int main() {
-    int V, E;
+int main()
+{
+  int V, E;
     cin >> V >> E;
-     int adj[1000][1000];
+
+    int adj[1000][1000];
     for(int i=0;i<V;i++)
     {
         for(int j=0;j<V;j++)
@@ -57,14 +81,16 @@ int main() {
         adj[b][a]=1;
 
     }
-    int a,b;
-    cin>>a>>b;
-    bool flag=hasPath(adj,V,a,b);
-    if(flag==true)
-        cout<<"true";
-    else
-        cout<<"false";
-
+    int v1,v2;
+    cin>>v1>>v2;
+    
+ vector<int>* output=bfs(V,adj,v1,v2);
+    if(output!=nullptr)
+    {
+        for (int i=0;i<output->size();i++)
+        {
+            cout<<output->at(i)<<" ";
+        }
+    }
   return 0;
 }
-
