f7f5e160dcefd60a33b0a65c41f863cc266deb7b
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2020-12-12 22:27:25 +0530

    Update prims algo.cpp

--- prims algo.cpp
+++ prims algo.cpp
@@ -1,1 +1,75 @@
 
+
+brightness_5
+// A C++ program for Prim's Minimum  
+// Spanning Tree (MST) algorithm. The program is  
+// for adjacency matrix representation of the graph  
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+// Number of vertices in the graph  
+#define V 5  
+  
+// A utility function to find the vertex with  
+// minimum key value, from the set of vertices  
+// not yet included in MST  
+int minKey(int key[], bool mstSet[])  
+{  
+    // Initialize min value  
+    int min = INT_MAX, min_index;  
+  
+    for (int v = 0; v < V; v++)  
+        if (mstSet[v] == false && key[v] < min)  
+            min = key[v], min_index = v;  
+  
+    return min_index;  
+}  
+  
+// A utility function to print the  
+// constructed MST stored in parent[]  
+void printMST(int parent[], int graph[V][V])  
+{  
+    cout<<"Edge \tWeight\n";  
+    for (int i = 1; i < V; i++)  
+        cout<<parent[i]<<" - "<<i<<" \t"<<graph[i][parent[i]]<<" \n";  
+}  
+  
+// Function to construct and print MST for  
+// a graph represented using adjacency  
+// matrix representation  
+void primMST(int graph[V][V])  
+{  
+   
+    int parent[V];  
+      
+  
+    int key[V];  
+      
+ 
+    bool mstSet[V];  
+  
+    for (int i = 0; i < V; i++)  
+        key[i] = INT_MAX, mstSet[i] = false;  
+  
+  x.  
+    key[0] = 0;  
+    parent[0] = -1; 
+    for (int count = 0; count < V - 1; count++) 
+    {  
+        
+        int u = minKey(key, mstSet);  
+  
+       
+        mstSet[u] = true;  
+  
+       
+        for (int v = 0; v < V; v++)  
+  
+           
+            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])  
+                parent[v] = u, key[v] = graph[u][v];  
+    }  
+  
+   
+    printMST(parent, graph);  
+}  
